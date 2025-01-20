4abe6b83fc98a49260f962620694dd2f8e432c92
duynstee <74192436+duynstee@users.noreply.github.com>
2025-01-20 15:14:24 +0100

    login

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,11 +1,34 @@
 <template>
-  <v-container>
-    <h1>Login Page</h1>
+  <v-container fluid fill-height class="background-image">
+    <v-row align="center" justify="center" class="fill-height">
+      <v-col cols="12" md="4">
+        <login-form></login-form>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import LoginForm from "@/components/LoginForm.vue";
+
 export default {
-  name: "LoginPage",
+  components: {
+    LoginForm,
+  },
 };
 </script>
+
+<style scoped>
+.v-container,
+.v-row {
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+}
+
+.background-image {
+  background-image: url("@/assets/krugerpark.jpg");
+  background-size: cover;
+  background-position: center;
+}
+</style>
