7d298d46be5a04ea77de523b30550da44d2ba26c
duynstee <olafgamemail@gmail.com>
2025-01-24 21:35:12 +0100

    url update

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -63,7 +63,7 @@ export default {
       }
 
       try {
-        const response = await axios.post("http://localhost:3000/login", {
+        const response = await axios.post("http://buoyprojectbackend.azurewebsites.net/login", {
           username: this.username,
           password: this.password,
         });
